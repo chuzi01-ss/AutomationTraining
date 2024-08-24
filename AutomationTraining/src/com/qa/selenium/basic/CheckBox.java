fcb1dd8c89bf36981839284336eed7ad6657e65b
chuzi01-ss <shivani01sep@gmail.com>
2024-08-24 20:07:48 +0530

    Changed variable name

--- AutomationTraining/src/com/qa/selenium/basic/CheckBox.java
+++ AutomationTraining/src/com/qa/selenium/basic/CheckBox.java
@@ -5,21 +5,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CheckBox {
-    static String checkbox_orange="input[type='checkbox'][value='blue']";
+    static String checkbox_Blue="input[type='checkbox'][value='blue']";
     static String appURL="https://omayo.blogspot.com/";
 
     public static void main (String[]args){
 
         WebDriver driver= new ChromeDriver();
         driver.get(appURL);
-        boolean checkboxSelected=driver.findElement(By.cssSelector(checkbox_orange)).isSelected();
+        boolean checkboxSelected=driver.findElement(By.cssSelector(checkbox_Blue)).isSelected();
 
 
         if(!checkboxSelected){
             System.out.println("Checkbox is selected or not");
 
-            driver.findElement(By.cssSelector(checkbox_orange)).click();
-            driver.findElement(By.cssSelector(checkbox_orange)).click();
+            driver.findElement(By.cssSelector(checkbox_Blue)).click();
+            driver.findElement(By.cssSelector(checkbox_Blue)).click();
         }
 
 
