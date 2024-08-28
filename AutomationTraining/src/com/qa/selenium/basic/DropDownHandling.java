877a4301e67f0d44aa3278c9ab3764a1c0809e10
chuzi01-ss <shivani01sep@gmail.com>
2024-08-29 01:05:38 +0530

    drop down changes

---
+++ AutomationTraining/src/com/qa/selenium/basic/DropDownHandling.java
@@ -0,0 +1,36 @@
+package com.qa.selenium.basic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropDownHandling {
+    static String appURL = "https://omayo.blogspot.com/";
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get(appURL);
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+
+        WebElement elm = driver.findElement(By.cssSelector("select#multiselect1"));
+        Select sel = new Select(elm);
+        sel.selectByIndex(0);
+        sel.selectByVisibleText("Audi");
+        sel.selectByValue("swiftx");
+
+
+        for (WebElement elem : sel.getAllSelectedOptions()) {
+            System.out.println(elem.getText());
+
+        }
+       // sel.deselectByIndex(0);
+       // sel.deselectByVisibleText("Audi");
+      //  sel.deselectByValue("swiftx");
+        sel.deselectAll();
+
+    }
+}
